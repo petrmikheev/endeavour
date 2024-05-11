0178209c4e71a89a7c7d3c8c7f363e8357986b15
Petr Mikheev <ptmikheev@gmail.com>
2024-05-11 19:53:50 +0200

    update gitignore

--- rtl/spinal/EndeavourSoc.v
+++ rtl/spinal/EndeavourSoc.v
@@ -1,6 +1,6 @@
 // Generator : SpinalHDL v1.10.1    git head : 2527c7c6b0fb0f95e5e1a5722a0be732b364ce43
 // Component : EndeavourSoc
-// Git hash  : fbe3330a66b63edfe47d192f4b369dc1f7547c47
+// Git hash  : a334dbe8469e37325f78dfcc1e1fee3597ee53d4
 
 `timescale 1ns/1ps
 
