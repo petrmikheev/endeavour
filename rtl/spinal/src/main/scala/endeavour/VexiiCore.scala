1fb7c1cef4cbe89c0415c4c11315b1d621314d24
Petr Mikheev <ptmikheev@gmail.com>
2024-12-02 19:56:10 +0100

    Update VexiiRiscv params

--- rtl/spinal/src/main/scala/endeavour/VexiiCore.scala
+++ rtl/spinal/src/main/scala/endeavour/VexiiCore.scala
@@ -2,13 +2,15 @@ package endeavour
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.com.jtag.Jtag
 import spinal.lib.bus.tilelink
 import spinal.lib.bus.tilelink.fabric.Node
 
 import vexiiriscv.ParamSimple
+import vexiiriscv.misc.EmbeddedRiscvJtag
 import vexiiriscv.soc.TilelinkVexiiRiscvFiber
 
-class VexiiCore(resetVector: Long, sim: Boolean = false) {
+class VexiiCore(resetVector: Long, sim: Boolean = false, jtag: Option[Jtag] = None) {
   val param = new ParamSimple()
   param.resetVector = resetVector
   param.xlen = 32
@@ -36,13 +38,32 @@ class VexiiCore(resetVector: Long, sim: Boolean = false) {
   param.privParam.withUser = true
   param.privParam.withRdTime = true
 
+  /*param.withRvf = true
+  param.fpuFmaFullAccuracy = false
+  param.fpuIgnoreSubnormal = true*/
+
+  // Performance optimization
   param.decoders = 2
   param.lanes = 2
+
+  param.fetchL1Ways = 4
+  param.lsuL1Ways = 4
+
+  param.fetchL1RefillCount = 3
+  param.lsuL1RefillCount = 4
+  param.lsuL1WritebackCount = 4
 
   param.withBtb = true
   param.withGShare = true
   param.withRas = true
 
+  param.fetchL1Prefetch = "nl"
+  //param.lsuSoftwarePrefetch = true
+  param.lsuHardwarePrefetch = "rpt"
+  param.lsuStoreBufferSlots = 4
+  param.lsuStoreBufferOps = 32
+
+  // fMax optimization
   param.relaxedBranch = true
   param.relaxedShift = true
   param.relaxedSrc = true
@@ -54,16 +75,11 @@ class VexiiCore(resetVector: Long, sim: Boolean = false) {
   param.allowBypassFrom = 0
   param.storeRs2Late = true
 
-  /*param.withRvf = true
-  param.fpuFmaFullAccuracy = false
-  param.fpuIgnoreSubnormal = true*/
-
-  // To consider
-  //fetchL1Prefetch
-  //lsuSoftwarePrefetch
-  //lsuHardwarePrefetch
-  //fetchL1Ways
-  //lsuL1Ways
+  if (jtag.isDefined) {
+    param.embeddedJtagTap = true
+    param.embeddedJtagCd = ClockDomain.current
+    param.privParam.withDebug = true
+  }
 
   val utime = UInt(64 bits)
 
@@ -73,7 +89,8 @@ class VexiiCore(resetVector: Long, sim: Boolean = false) {
     val external = Bool()
   }
 
-  val core = new TilelinkVexiiRiscvFiber(param.plugins())
+  val plugins = param.plugins()
+  val core = new TilelinkVexiiRiscvFiber(plugins)
   core.priv match {
     case Some(priv) => new Area {
       val mti, msi, mei, sei = misc.InterruptNode.master()
@@ -90,10 +107,51 @@ class VexiiCore(resetVector: Long, sim: Boolean = false) {
     case None =>
   }
 
+  if (jtag.isDefined) {
+    plugins.foreach{
+      case p : EmbeddedRiscvJtag => { fiber.Handle { jtag.get <> p.logic.jtag } }
+      case _ =>
+    }
+  }
+
   core.iBus.setDownConnection { (down, up) =>
     down.a << up.a.halfPipe().halfPipe()
     up.d << down.d.m2sPipe()
   }
   core.lsuL1Bus.setDownConnection(a = StreamPipe.HALF, b = StreamPipe.HALF_KEEP, c = StreamPipe.FULL, d = StreamPipe.M2S_KEEP, e = StreamPipe.HALF)
   core.dBus.setDownConnection(a = StreamPipe.HALF, d = StreamPipe.M2S_KEEP)
+}
+
+class VexiiMinimalCore(resetVector: Long) {
+  val param = new ParamSimple()
+  param.resetVector = resetVector
+  param.xlen = 32
+  param.hartCount = 1
+  param.withMul = true
+  param.withDiv = true
+  param.privParam.withRdTime = true
+
+  val utime = UInt(64 bits)
+
+  val interrupts = new Bundle {
+    val timer = Bool()
+    val software = Bool()
+    val external = Bool()
+  }
+
+  val plugins = param.plugins()
+  val core = new TilelinkVexiiRiscvFiber(plugins)
+  core.priv match {
+    case Some(priv) => new Area {
+      val mti, msi, mei = misc.InterruptNode.master()
+      mti.flag := interrupts.timer
+      msi.flag := interrupts.software
+      mei.flag := interrupts.external
+      priv.mti << mti
+      priv.msi << msi
+      priv.mei << mei
+      priv.rdtime := utime
+    }
+    case None =>
+  }
 }
